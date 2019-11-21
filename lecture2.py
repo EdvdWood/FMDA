7b0a56dd99a6cb2038816cd251e2fb5156d91ea6
Sagarmatra <ejvdhout@vivaldi.net>
2019-11-21 12:10:36 +0100

    done with lecture 2

--- lecture2.py
+++ lecture2.py
@@ -65,5 +65,36 @@ print(runner()[0], runner()[1])
 
 #%%
 
+def leapyear (year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return ("The year {0} is a leap year".format(year)) 
+    else:
+        return ("The year {0} is not a leap year".year(year))
+    
+print(leapyear(400))
+
+#%%
+
+def fibonacci(runs, max):
+    fiboList = [1, 1]
+    
+    for i in range(runs):
+        fiboList = fiboList + [fiboList[-1]+fiboList[-2]]
+        if fiboList[-1] > max:
+            return fiboList[0:-1]
+    return fiboList
+
+printed = fibonacci(1000, 1000000)
+
+print(printed, len(printed))
+
+#%%
+
+def cube (number):
+    number = int(number)
+    return number**3
+
+print(cube(3))
+        
 
     
